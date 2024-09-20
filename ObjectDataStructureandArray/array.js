dc041a34e2e3da5518fa74723ac674dbd916a4c6
ArefinatWORK <arefinatwork@gmail.com>
2024-09-20 19:35:26 +0600

    just simply submit bro

---
+++ ObjectDataStructureandArray/array.js
@@ -0,0 +1,6 @@
+var myself = ["Ayan","Codingal student",14,"play Virtual PC games","Creative and Efficient Learner","do Coding"]
+console.log("I'm " + myself[0] + ".")
+console.log("I'm " + myself[2] + " years old.")
+console.log("My hobby is to " + myself[3] + " and " + myself[5] + ".")
+console.log("I'm proud to be a " + myself[1] + ".")
+console.log("My teacher indicates me as a " + myself[4] + ".")
---
+++ ObjectDataStructureandArray/multiplication.js
@@ -0,0 +1,4 @@
+var maths = ["1","2","3","4","5"]
+var answer = 1*2*3*4*5;
+console.log("Do you know what is the answer of the Multiplication of " + maths + "?")
+console.log("The answer of " + maths.join("*") + " is" + " = " + answer)
