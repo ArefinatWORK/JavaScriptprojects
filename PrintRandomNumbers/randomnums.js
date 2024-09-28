5033be8f6a0249c6f28fb4f7ec1128b2b0572f68
ArefinatWORK <arefinatwork@gmail.com>
2024-09-28 12:06:45 +0600

    submit

---
+++ PrintRandomNumbers/randomnums.js
@@ -0,0 +1,13 @@
+console.log('Today we will be printing RANDOM Numbers using the "let" Variable and the "while" Loop;)')
+
+{
+let randomnumber;
+let i = 0;
+
+while (i < 10) { 
+  randomnumber = Math.random();
+  i++;
+  console.log(randomnumber);
+}
+}
+console.log("Look!","I printed 10 RANDOM Numbers!")
